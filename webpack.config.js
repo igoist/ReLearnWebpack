935b55eb7f979d561c2812d7081d22026d4dade3
igoist <493512132@qq.com>
2017-07-13 23:52:33 +0800

    dev: what's hell of HMR?

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,62 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   devtool: "cheap-eval-source-map",
   // devtool: "inline-source-map",
   entry: {
     app: './src/index.js',
-    vender: ['lodash']
+    // vender: ['lodash']
+    vendor: ['react', 'react-dom']
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    // new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management',
       filename: 'index.html',
       template: 'src/index.html'
     })
   ],
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       use: [
-  //         'style-loader',
-  //         'css-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.(png|svg|jpg|gif)$/,
-  //       use: [
-  //         'file-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
-  //       use: [
-  //         'file-loader'
-  //       ]
-  //     }
-  //   ]
-  // }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    port: 8321,
+    hot: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
 };
